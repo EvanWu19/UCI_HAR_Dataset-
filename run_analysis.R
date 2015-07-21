98ae4c4ff67f6ac44f6201950b69d51bee98c7a5
eva891022 <eva891022@gmail.com>
2015-07-21 18:06:52 -0400

    finish step 2

--- run_analysis.R
+++ run_analysis.R
@@ -7,8 +7,13 @@ rawtrain<-cbind(
       read.table("./UCI HAR Dataset/train/y_train.txt"),
       read.table("./UCI HAR Dataset/train/x_train.txt"))
 rawtotal<-rbind(rawtest,rawtrain)
+## finish Merges the training and the test sets to create one data set.
 features<-as.character(read.table("./UCI HAR Dataset/features.txt")[,2])
-rawlabels<-c("subject","activity_labels")
+rawlabels<-c("subject_","activity_labels")
 rawlabels1<-append(rawlabels,features)
 colnames(rawtotal)<-rawlabels1
-head(rawtotal,3)
+datastep2<-rawtotal[,c(1,
+                       2,
+                       grep("-mean()", colnames(rawtotal),fixed=T),
+                       grep("-std()", colnames(rawtotal),fixed=T))]
+## 
